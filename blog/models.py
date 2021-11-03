b97a83646175ac8615018df8473c8831d42c9f5e
sabbir2609 <sabbirhasan2999@gmail.com>
2021-11-03 14:36:36 +0600

    some thoughts on pipenv

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 from django.db.models.fields import EmailField
 from django.utils import timezone
@@ -62,65 +61,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
-=======
-from django.db import models
-from django.db.models.fields import EmailField
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.urls import reverse
-
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
-
-
-class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
-    )
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blog_posts')
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default='draft')
-
-    objects = models.Manager()  # The default manager
-    published = PublishedManager()  # our custom manager
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day, self.slug])
-
-    class Meta:
-        ordering = ('-publish', )
-        verbose_name_plural = 'Post'
-
-    def __str__(self):
-        return self.title
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ('created',)
-
-    def __str__(self):
-        return f'Comment by {self.name} on {self.post}'
->>>>>>> d22aa44b935afa022f1b86becc68acaedf2bb204
